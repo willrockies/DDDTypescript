64193648ab0de521f74b61d3211d650cce9ec335
willrockies <wlsn.ads@gmail.com>
2023-04-19 12:27:44 -0300

    tests not working

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -16,11 +16,11 @@
  */
 import Address from './address'
 export default class Customer {
-  _id: string;
-  _name: string = "";
+  private _id: string;
+  private _name: string = "";
   //_address: string = ""; // atributo sem expressividade, tipo primario
-  _address!: Address;
-  _active: boolean = false;
+  private _address!: Address;
+  private _active: boolean = false;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -36,13 +36,18 @@ export default class Customer {
     this._name = name;
   } */
 
+  get name(): string{
+    return this._name;
+  }
+
   //entidade sempre vai se autovalidar
   validate() {
-    if (this._name.length === 0) {
-      throw new Error('Name is required');
-    }
     if (this._id.length === 0) {
       throw new Error('Id is required');
+    }
+
+    if (this._name.length === 0) {
+      throw new Error('Name is required');
     }
   }
 
